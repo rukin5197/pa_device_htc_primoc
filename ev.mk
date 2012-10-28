4bb8ace69cd26bb8f6b2dfafb1293ed4b59bf833
jmztaylor <jmztaylor@gmail.com>
2012-10-28 10:20:00 -0300

    change codename

--- ev.mk
+++ ev.mk
@@ -15,7 +15,7 @@ PRODUCT_MANUFACTURER := HTC
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=htc_primoc BUILD_FINGERPRINT=virgin_mobile/htc_primoc/primou:4.1.1/JRO03L/330937:user/release-keys PRIVATE_BUILD_DESC="4.1.1 JRO03L 330937 release-keys"
 
 # Set up the product codename, build version & MOTD.
-PRODUCT_CODENAME := Acies
+PRODUCT_CODENAME := Quanto
 PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your PrimoC\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @jmz_spftware for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
