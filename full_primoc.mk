0c9a469b891d61143618991fad609fe270933562
Gannon Macdonald <gannon050197@gmail.com>
2012-11-04 11:45:13 -0600

    full_primoc: remove the device.mk

--- full_primoc.mk
+++ full_primoc.mk
@@ -29,9 +29,6 @@ PRODUCT_PACKAGES := \
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from primou device
-$(call inherit-product, device/htc/primoc/device.mk)
-
 # Set those variables here to overwrite the inherited values.
 PRODUCT_NAME := full_primoc
 PRODUCT_DEVICE := primoc
